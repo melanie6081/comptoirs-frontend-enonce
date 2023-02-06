930743ab1984745cf2eebcded45428b250c19aa3
melanie <melanie.feron@etud.univ-jfc.fr>
2023-02-06 11:21:05 +0000

    final push

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import CategorieView from '../views/CategorieView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import CategorieView from "../views/CategorieView.vue";
+import ProduitView from "../views/ProduitView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-    // Associe des chemins d'accès au composant vue à afficher
+  // Associe des chemins d'accès au composant vue à afficher
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/categories',
-      name: 'categories',
-      component: CategorieView
+      path: "/categories",
+      name: "categories",
+      component: CategorieView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/produits",
+      name: "produits",
+      component: ProduitView,
+    },
+    {
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
